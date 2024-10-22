7fbe8a2e506dfbbfaea70bb986ebde5bfe6ffc20
Ravi Raj <330993@conneqtcorp.com>
2024-10-22 17:41:09 +0530

    placeholder concept

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,60 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
-export default function decorate(block) {
+import {
+  createOptimizedPicture,
+  fetchPlaceholders,
+} from '../../scripts/aem.js';
+ 
+export default async function decorate(block) {
+  // fetch placeholders from the root folder
+  const placeholders = await fetchPlaceholders('');
+  console.log(placeholders);
+  const { clickHereForMore, clickHereForLess } = placeholders;
+  console.log(clickHereForLess);
+ 
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+        const paragraphs = div.querySelectorAll('p');
+        if (paragraphs.length > 1) {
+          console.log(paragraphs[1]);
+          const secondParagraph = paragraphs[1];
+          // Initially show the second paragraph with ellipsis (CSS should already be handling this)
+          secondParagraph.classList.add('truncated'); // Apply CSS class for ellipsis
+          // Create a button below the second paragraph
+          const clickformorebtn = document.createElement('a');
+          clickformorebtn.innerText = clickHereForMore;
+          clickformorebtn.classList.add('button');
+          console.log(clickformorebtn);
+          secondParagraph.insertAdjacentElement('afterend', clickformorebtn);
+          // Target the existing anchor tag in the HTML
+          const placeholderLink = div.querySelector('.button');
+          console.log(placeholderLink);
+          placeholderLink.setAttribute('href', '#');
+          function placeholderClick(e) {
+            e.preventDefault();
+            // Toggle between showing full content and truncating with ellipsis
+            if (secondParagraph.classList.contains('truncated')) {
+              secondParagraph.classList.remove('truncated');
+              secondParagraph.style.display = 'block';
+              placeholderLink.innerText = clickHereForLess || 'Click here for less'; // Change to 'Click here for less'
+            } else {
+              secondParagraph.classList.add('truncated');
+              placeholderLink.innerText = clickHereForMore || 'Click here for more'; // Revert to 'Click here for more'
+            }
+          };
+          placeholderLink.addEventListener('click', placeholderClick)
+        }
+      }
     });
     ul.append(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }],)));
   block.textContent = '';
   block.append(ul);
 }
